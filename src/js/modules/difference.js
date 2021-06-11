9e63fb2ef3df4208113e22e56a4faa1891f66139
Stas Kukarkin <staskykarkin@gmail.com>
2021-06-11 11:53:36 +0600

    slider of another page

--- src/js/modules/difference.js
+++ src/js/modules/difference.js
@@ -1,6 +1,6 @@
 export default class Difference {
-    constructor(oldOfficer, newOfficer, items){
-        try{
+    constructor(oldOfficer, newOfficer, items) {
+        try {
             this.oldOfficer = document.querySelector(oldOfficer);
             this.newOfficer = document.querySelector(newOfficer);
             this.items = items;
@@ -8,34 +8,35 @@ export default class Difference {
             this.newItems = this.newOfficer.querySelectorAll(items);
             this.oldCounter = 0;
             this.newCounter = 0;
-        } catch(e){}
+        } catch (e) {}
     }
 
-    bindTriggers(container,items, counter ){
-        container.querySelector('.plus').addEventListener('click', () =>{
-            if (counter !== items.length - 2){
+    bindTriggers(container, items, counter) {
+        container.querySelector('.plus').addEventListener('click', () => {
+            if (counter !== items.length - 2) {
                 items[counter].style.display = 'flex';
-                counter ++;
-            } else{
+                counter++;
+            } else {
                 items[counter].style.display = 'flex';
                 items[items.length - 1].remove();
             }
         });
     }
 
-    hideItems(items){
-        items.forEach((item, i, arr) =>{
+    hideItems(items) {
+        items.forEach((item, i, arr) => {
             if (i !== arr.length - 1) {
                 item.style.display = 'none';
             }
         });
     }
 
-    init(){
-        try{        this.hideItems(this.oldItems);
+    init() {
+        try {
+            this.hideItems(this.oldItems);
             this.hideItems(this.newItems);
-            this.bindTriggers(this.oldOfficer, this.oldItems,this.oldCounter);
-            this.bindTriggers(this.newOfficer, this.newItems,this.newCounter);
-            } catch(e){}
-    } 
+            this.bindTriggers(this.oldOfficer, this.oldItems, this.oldCounter);
+            this.bindTriggers(this.newOfficer, this.newItems, this.newCounter);
+        } catch (e) {}
+    }
 }
